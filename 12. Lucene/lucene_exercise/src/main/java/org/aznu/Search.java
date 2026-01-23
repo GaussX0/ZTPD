2b457da902fa1220c5946ea8bc7c2390835f27b3
gaussx <kmalecki2002@gmail.com>
2026-01-23 15:35:12 +0100

    Finish exercise 12

---
+++ 12. Lucene/lucene_exercise/src/main/java/org/aznu/Search.java
@@ -0,0 +1,42 @@
+package org.aznu;
+
+import org.apache.lucene.analysis.pl.PolishAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class Search {
+    private static final String INDEX_DIRECTORY = "./index";
+
+    public static void main(String[] args) throws IOException, ParseException {
+        PolishAnalyzer analyzer = new PolishAnalyzer();
+        Directory directory = FSDirectory.open(Paths.get(INDEX_DIRECTORY));
+        IndexReader reader = DirectoryReader.open(directory);
+        IndexSearcher searcher = new IndexSearcher(reader);
+        String querystr = "*:*";
+        Query q = new QueryParser("title", analyzer).parse(querystr);
+        int maxHits = 10;
+        TopDocs docs = searcher.search(q, maxHits);
+        ScoreDoc[] hits = docs.scoreDocs;
+        System.out.println("Znaleziono " + hits.length + " dokumentów dla frazy: '" + querystr + "'");
+        for(int i=0; i<hits.length; ++i) {
+            int docId = hits[i].doc;
+            Document d = searcher.doc(docId);
+            System.out.println((i + 1) + ". " + d.get("isbn") + "\t" + d.get("title"));
+        }
+        reader.close();
+        directory.close();
+    }
+}
